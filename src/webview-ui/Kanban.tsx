5f9a4ad3a7905ca94c829b523a18261cf1cb0b3b
eSuminski <rctolinski@gmail.com>
2025-07-10 21:54:17 -0400

    breaking down board into smaller components

--- src/webview-ui/Kanban.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Kanban: React.FC = () => (
-  <div className="kanban">
-    <div className="column"><h2>TODO</h2></div>
-    <div className="column"><h2>DOING</h2></div>
-    <div className="column"><h2>DONE</h2></div>
-  </div>
-);
--- src/webview-ui/index.tsx
+++ src/webview-ui/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Kanban } from './Kanban';
-import './style.css';
+import { Kanban } from './board/Kanban';
+import './styling/style.css';
 
 const root = document.getElementById('root');
 if (root) {
