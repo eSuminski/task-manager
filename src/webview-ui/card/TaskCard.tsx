87870d17d6bb4342d94912c1182acaaf80db1544
eSuminski <rctolinski@gmail.com>
2025-11-01 16:37:57 -0500

    adding subtasks to task cards

--- src/webview-ui/card/TaskCard.tsx
+++ src/webview-ui/card/TaskCard.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { Task } from "../interface/KanbanTaskProps";
 
-interface TaskCardProps {
-  title: string;
-  description?: string;
-}
 
-export const TaskCard: React.FC<TaskCardProps> = ({ title, description }) => (
+export const TaskCard: React.FC<Task> = ({ title, description, subtasks }) => (
   <div className="task-card">
     <div className="task-card-title">{title}</div>
-    {description && (
-      <div className="task-card-description">{description}</div>
+    {description && (<div className="task-card-description">{description}</div>)}
+    {subtasks && subtasks.length > 0 && (
+      <ul className="task-card-subtasks">
+        {subtasks.map((subtask,idx)=> (
+          <li key={idx}>
+            {subtask.title}
+            <input type="checkbox" checked={subtask.completed}/>
+          </li>
+        ))}
+      </ul>
     )}
   </div>
 );
